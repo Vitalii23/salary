d66e3993c7a29ad9083895c38b830354682b1dd8
КАПЕ\User <danilov.vit.an@gmail.com>
2017-06-17 12:46:54 +0700

    Adden tag h1

---
+++ salary/salary/UnitTests/model/MyClassTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using salary;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace UnitTests.model
+{
+    [TestFixture]
+    public class MyClassTest
+    {
+
+        [Test]
+       [TestCase(12, 1, TestName = "Тестирование Count при присваивании 12,1,")]
+
+        /// [TestCase(5,0, TestName = "Тестирование Count при присваивании 5,0")]
+        /// [TestCase(int.MaxValue, TestName = "Тестирование Count при присваивании MaxValue.")]
+        /// [TestCase(int.MaxValue - 1, TestName = "Тестирование Count при присваивании MaxValue - 1.")]
+        /// [TestCase(-1, ExpectedResult = typeof(ArgumentException), TestName = "Тестирование Count при присваивании - 1.")]
+        /// [TestCase(int.MinValue, ExpectedResult = typeof(ArgumentException), TestName = "Тсетирование Count приприсваивании минимально допустимого целого числа.")]
+       public void CountTest(decimal count1, decimal count2)
+        {
+            var EmployerHour = new EmployerHour();
+            EmployerHour.PayPerHour = count1;
+            EmployerHour.HourInMonth = count2;
+
+        }
+
+
+        [Test]
+        [TestCase("Гитлер", "Адольф", TestName = "Тестирование Count при присваивании Гитлер, Адольф")]
+        public void CountTest1(string count3, string count4)
+        {
+            var EmployerMonth = new EmployerMonth();
+            EmployerMonth.Name = count3;
+            EmployerMonth.Surname = count4;
+
+        }
+
+    }
+}
---
+++ salary/salary/salary/Addemployee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salary
+{
+    public partial class Form2 : Form
+    {
+        // public IEmployer Employer {
+        //   get
+        // {
+        //   var employer = new EmplployeeHour();
+        // employer.Name = textBoxName.Text;
+        // return employer;
+        // } }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            textBoxName.Text = "";
+            textBoxSurname.Text = "";
+            textBoxPatronymic.Text = "";
+            numericUpDownHourInMonth.Value = 0;
+            numericUpDownPayPerHour.Value = 0;
+            numericUpDownPayPerMonth.Value = 0;
+            dateTimePickerDateBirthday.Value = DateTime.Now;
+        }
+
+        private void textBoxSurname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char l = e.KeyChar;
+            if ((l < 'А' || l > 'я') && l != '\b' && l != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char l = e.KeyChar;
+            if ((l < 'А' || l > 'я') && l != '\b' && l != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxPatronymic_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char l = e.KeyChar;
+            if ((l < 'А' || l > 'я') && l != '\b' && l != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void numericUpDownPayPerHour_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void numericUpDownHourInMonth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void numericUpDownPayPerMonth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
